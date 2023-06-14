192a5b2a16f9f20c8123d4efd71533e5336921e0
aidenlu <aiden_lu@wochacha.com>
2023-06-14 10:29:35 +0800

    fix: skip downloading default avatar

--- casdoor_auth/utils.py
+++ casdoor_auth/utils.py
@@ -5,12 +5,16 @@ from django.core.files.base import ContentFile
 
 
 def save_avatar_from_url(user, url):
+    parsed_url = urllib.parse.urlparse(url)
+    filename = os.path.basename(parsed_url.path)
+    file_ext = os.path.splitext(filename)[1]
+
+    if filename == "casbin.svg":
+        return
+
     response = requests.get(url)
     if response.status_code != 200:
         print("downloading avatars failed")
         return
 
-    parsed_url = urllib.parse.urlparse(url)
-    filename = os.path.basename(parsed_url.path)
-    file_ext = os.path.splitext(filename)[1]
     user.avatar.save(f"{user.pk}{file_ext}", ContentFile(response.content), save=True)
