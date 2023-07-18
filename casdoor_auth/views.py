a845265b8a8faf71bceadf2a7a234af89d1a4d49
aidenlu <aiden_lu@wochacha.com>
2023-07-18 14:28:38 +0800

    refactor: not set avatar

--- casdoor_auth/views.py
+++ casdoor_auth/views.py
@@ -28,13 +28,15 @@ User = get_user_model()
 
 conf = settings.CASDOOR_CONFIG
 
-sdk = CasdoorSDK(conf.get('endpoint'),
-                 conf.get('client_id'),
-                 conf.get('client_secret'),
-                 conf.get('certificate'),
-                 conf.get('org_name'),
-                 conf.get('application_name'),
-                 conf.get('endpoint'))
+sdk = CasdoorSDK(
+    conf.get("endpoint"),
+    conf.get("client_id"),
+    conf.get("client_secret"),
+    conf.get("certificate"),
+    conf.get("org_name"),
+    conf.get("application_name"),
+    conf.get("endpoint"),
+)
 
 
 @csrf_exempt
@@ -45,7 +47,7 @@ def to_login(request):
 
 @csrf_exempt
 def to_logout(request):
-    id_token = request.session.get('casdoor_token')
+    id_token = request.session.get("casdoor_token")
     logout(request)
     redirect_url = sdk.get_auth_link(redirect_uri=settings.REDIRECT_URI)
     logout_url = f"{sdk.front_endpoint}/api/logout?id_token_hint={id_token}&post_logout_redirect_uri={redirect_url}"
@@ -54,25 +56,25 @@ def to_logout(request):
 
 @csrf_exempt
 def callback(request):
-    code = request.GET.get('code')
+    code = request.GET.get("code")
     token = sdk.get_oauth_token(code)
     if isinstance(token, dict):
         token = token.get("access_token")
 
     user = sdk.parse_jwt_token(token)
-    request.session['user'] = user
-    email = user.get('email')
-    username = user.get('name')
-    display_name = user.get('displayName', username)
-    is_admin = user.get('isAdmin', False)
-    avatar_url = user.get('avatar', '')
-    phone = user.get('phone', '')
+    request.session["user"] = user
+    email = user.get("email")
+    username = user.get("name")
+    display_name = user.get("displayName", username)
+    is_admin = user.get("isAdmin", False)
+    avatar_url = user.get("avatar", "")
+    phone = user.get("phone", "")
     in_user = None
     user_fields = [f.name for f in User._meta.fields]
 
     if email:
         try:
-            in_user = User.objects.get(email=user.get('email'))
+            in_user = User.objects.get(email=user.get("email"))
         except User.MultipleObjectsReturned:
             raise ValueError(f"Multiple emails found: {email}")
         except User.DoesNotExist:
@@ -88,17 +90,19 @@ def callback(request):
 
     if not in_user:
         extra_fields = dict(is_superuser=is_admin, is_staff=is_admin)
-        in_user = User.objects.create_user(username, email=email, name=display_name, **extra_fields)
-    
-    if 'avatar' in user_fields and avatar_url and not bool(in_user.avatar):
-        save_avatar_from_url(in_user, avatar_url)
-    
-    if 'phone' in user_fields and phone and not in_user.phone:
+        in_user = User.objects.create_user(
+            username, email=email, name=display_name, **extra_fields
+        )
+
+    # if "avatar" in user_fields and avatar_url and not bool(in_user.avatar):
+    #    save_avatar_from_url(in_user, avatar_url)
+
+    if "phone" in user_fields and phone and not in_user.phone:
         in_user.phone = phone
-        in_user.save(update_fields=['phone'])
+        in_user.save(update_fields=["phone"])
 
     login(request, in_user)
-    request.session['casdoor_token'] = token
+    request.session["casdoor_token"] = token
 
     redirect_url = settings.LOGIN_REDIRECT_URL + f"?username={in_user.username}"
     return redirect(redirect_url)
@@ -106,25 +110,25 @@ def callback(request):
 
 @csrf_exempt
 def callback_no_redirect(request):
-    code = request.GET.get('code')
+    code = request.GET.get("code")
     token = sdk.get_oauth_token(code)
     if isinstance(token, dict):
         token = token.get("access_token")
 
     user = sdk.parse_jwt_token(token)
-    request.session['user'] = user
-    email = user.get('email')
-    username = user.get('name')
-    display_name = user.get('displayName', username)
-    is_admin = user.get('isAdmin', False)
-    avatar_url = user.get('avatar', '')
-    phone = user.get('phone', '')
+    request.session["user"] = user
+    email = user.get("email")
+    username = user.get("name")
+    display_name = user.get("displayName", username)
+    is_admin = user.get("isAdmin", False)
+    avatar_url = user.get("avatar", "")
+    phone = user.get("phone", "")
     in_user = None
     user_fields = [f.name for f in User._meta.fields]
 
     if email:
         try:
-            in_user = User.objects.get(email=user.get('email'))
+            in_user = User.objects.get(email=user.get("email"))
         except User.MultipleObjectsReturned:
             raise ValueError(f"Multiple emails found: {email}")
         except User.DoesNotExist:
@@ -140,16 +144,17 @@ def callback_no_redirect(request):
 
     if not in_user:
         extra_fields = dict(is_superuser=is_admin, is_staff=is_admin)
-        in_user = User.objects.create_user(username, email=email, name=display_name, **extra_fields)
-    
-    if 'avatar' in user_fields and avatar_url and not bool(in_user.avatar):
-        save_avatar_from_url(in_user, avatar_url)
-    
-    if 'phone' in user_fields and phone and not in_user.phone:
+        in_user = User.objects.create_user(
+            username, email=email, name=display_name, **extra_fields
+        )
+
+    # if 'avatar' in user_fields and avatar_url and not bool(in_user.avatar):
+    #    save_avatar_from_url(in_user, avatar_url)
+
+    if "phone" in user_fields and phone and not in_user.phone:
         in_user.phone = phone
-        in_user.save(update_fields=['phone'])
+        in_user.save(update_fields=["phone"])
 
     login(request, in_user)
-    request.session['casdoor_token'] = token
+    request.session["casdoor_token"] = token
     return HttpResponse("ok")
-
